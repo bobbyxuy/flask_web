703540cd407231c3a11a02fec69489a71d794fe7
bobbyxuy <xyz1219@qq.com>
2017-05-31 03:00:37 +0800

    none
    none

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.qq.com'
     MAIL_PORT = 587
-    MAIL_USE_LLS = True
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
